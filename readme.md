d057ab8e9ea6c938ed261889d45d8da3fd48dcc4
lucas machado <lucas.figueira@gmail.com>
2017-02-18 18:49:10 -0200

    feat(text): reverse string

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-Mega Project List
+Mega Project List - Python
 ========
 
 A list of practical projects made in python, based on this [repository](https://github.com/karan/Projects).
---
+++ reverse_string.py
@@ -0,0 +1,6 @@
+#########################################################################################
+# Reverse a String - Enter a string and the program will reverse it and print it out. #
+#########################################################################################
+
+input_string = raw_input("Type a string: ")
+print input_string[::-1]
